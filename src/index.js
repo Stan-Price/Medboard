c769d44c28059562068cd11d468cf5d4fb9a6219
Stan-Price <stanpricefree@gmail.com>
2023-02-16 10:58:30 +0200

    first upload

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// Css
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/animate.css/animate.css';
+import '../node_modules/slick-carousel/slick/slick.css';
+import '../node_modules/slick-carousel/slick/slick-theme.css';
+import '../node_modules/cropperjs/dist/cropper.css';
+import '../node_modules/toastr/build/toastr.min.css';
+import '../node_modules/react-perfect-scrollbar/dist/css/styles.css';
+import '../node_modules/react-data-table-component-extensions/dist/index.css';
+import '../node_modules/driver.js/dist/driver.min.css';
+import '../node_modules/devextreme/dist/css/dx.common.css';
+import '../node_modules/devextreme/dist/css/dx.light.css';
+import './assets/vendors/iconic-fonts/flat-icons/flaticon.css';
+import './assets/vendors/iconic-fonts/cryptocoins/cryptocoins.css';
+import './assets/vendors/iconic-fonts/cryptocoins/cryptocoins-colors.css';
+import './assets/vendors/iconic-fonts/font-awesome/css/all.min.css';
+import './assets/css/style.css';
+
+ReactDOM.render(
+  <BrowserRouter basename={'/templatemonster/react/medboard'}>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById('medboard')
 );
 
 // If you want to start measuring performance in your app, pass a function
